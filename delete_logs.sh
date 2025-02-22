e5c1026de3b38dd658c5f0739071862e2537252e
nirshang <ngodugunuri@gmail.com>
2025-02-21 19:23:26 -0700

    kk

--- delete_logs.sh
+++ delete_logs.sh
@@ -6,3 +6,7 @@ SOURCE_DIR="/home/ec2-user/app-logs"
 Files_to_delete=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
 echo "Files to delete: $Files_to_delete"
+while read -r file
+do 
+   echo "deleting file: $file"
+done <<< $Files_to_delete
